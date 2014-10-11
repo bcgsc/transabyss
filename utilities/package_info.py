29edd1799988392551582d6f0f6ca086f92a1aff
kmnip <kamingnip@gmail.com>
2014-10-10 17:39:38 -0700

    consolidate package version info

---
+++ utilities/package_info.py
@@ -0,0 +1,7 @@
+VERSION = "1.5.2"
+NAME = "Trans-ABySS"
+SUPPORT_INFO = '''\
+Written by Ka Ming Nip.
+Copyright 2014 Canada's Michael Smith Genome Sciences Centre
+Report bugs to <trans-abyss@googlegroups.com>
+'''
