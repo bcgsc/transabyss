0909d5acfcdcb87a64099f38ed8340f756965fa4
kmnip <kamingnip@gmail.com>
2015-05-21 10:16:35 -0700

    update package info

--- utilities/package_info.py
+++ utilities/package_info.py
@@ -1,10 +1,10 @@
 import os
 
-VERSION = "1.5.2"
+VERSION = "1.5.3"
 NAME = "Trans-ABySS"
 SUPPORT_INFO = '''\
 Written by Ka Ming Nip.
-Copyright 2014 Canada's Michael Smith Genome Sciences Centre
+Copyright 2015 Canada's Michael Smith Genome Sciences Centre
 Report bugs to <trans-abyss@googlegroups.com>
 '''
 PACKAGEDIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
